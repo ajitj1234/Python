a12a7caf71a6e79410a56ca1b07db4bb80cf6e33
ajitj1234 <38103833+ajitj1234@users.noreply.github.com>
2018-04-23 18:33:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-print("Hello Ajit")
-print("My name is" + "Hello:Nick:World".split(":") [1])
-print("raj")
-print("This costs" +str(6)+"dollors")
-print("This costs" +str("6+5")+"dollors")
-print("This costs " + str(6+5) +" dollors")
-print(("Hello:Nick:World").split(":"))
-print("My name is"+"Hello:nick:world".split(":")[2])
+from battleship import Person, bcolors
 
-print("My name is"+"Hello:nick:world".split(":")[0])
-print("Hello:Nick:World".split(":"))
-print(int(5.5))
-print(str(5.5))
-print(5+6)
-print("5+6")
-print("5" + "6")
-print("This costs" +str(6+5)+"dollors")
-print(int(5) + int(6))
-print(int("5") + int("6"))
-print(int(5.5)+int(4.5))
-print(5.5+4.5)
-print(int(5.5+4.5))
-print(float(5.5))
-print(float(int(5.5+4.5)))
-print(float(str(int(5.5))))
-print(int(float(5.5+4.5)))
-print(float(int(5+9)))
-print("Addition of two floatimg no.s is (int(5.5)+int(4.5))")
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "dragon", "cost": 10, "dmg": 80},
+         {"name": "hippo", "cost": 10, "dmg": 70}]
+
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY attacks" + bcolors.ENDC)
+
+while running:
+   # player.choose_action()
+    player.choose_magic()
+
+    choice = input("choose action:")
+    index = int(choice) - 1
+    #print("You chose:", choice)
+    print("you chose", player.get_spell_name(int(index)))
+
+    running = False
+#print(player.generate_damage())
+#print(player.generate_damage())
+#print(player.generate_damage())
+
+#print(player.generate_spell_damage(0))
